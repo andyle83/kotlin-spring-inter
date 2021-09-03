603d91a9a94bedbead496cac699f25289ed275a6
Tuan Anh Le <anh.le@myob.com>
2021-09-03 20:49:52 +1000

    create unit test for bank controller

--- src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/controllers/BankController.kt
+++ src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/controllers/BankController.kt
@@ -1,7 +1,15 @@
 package tv.codealong.tutorials.springboot.thenewboston.controllers
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import tv.codealong.tutorials.springboot.thenewboston.service.BankService
 
 @RestController
-class BankController {
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks() = service.getBanks()
+
 }
--- src/test/kotlin/tv/codealong/tutorials/springboot/thenewboston/controllers/BankControllerTest.kt
+++ src/test/kotlin/tv/codealong/tutorials/springboot/thenewboston/controllers/BankControllerTest.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,13 +17,10 @@ internal class BankControllerTest {
 
     @Test
     fun `should return all banks`() {
-        // given
-        
-        
-        // when
-        
-        
-        // then
-        
+        // when / then
+        mockMvc.get("/api/banks")
+            .andDo { print() }
+            .andExpect { status { isOk() } }
     }
+
 }
