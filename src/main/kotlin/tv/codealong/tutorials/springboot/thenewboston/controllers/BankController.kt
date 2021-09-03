a92810319e07ccb71302ceffd587815609f59de6
Tuan Anh Le <anh.le@myob.com>
2021-09-04 08:53:09 +1000

    write enough code to pass the test for 404

--- src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/controllers/BankController.kt
+++ src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/controllers/BankController.kt
@@ -1,14 +1,16 @@
 package tv.codealong.tutorials.springboot.thenewboston.controllers
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 import tv.codealong.tutorials.springboot.thenewboston.service.BankService
 
 @RestController
 @RequestMapping("/api/banks")
 class BankController(private val service: BankService) {
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> = ResponseEntity(e.message, HttpStatus.NOT_FOUND)
 
     @GetMapping
     fun getBanks() = service.getBanks()
