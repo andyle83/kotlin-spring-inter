50ab2351c4769b361e0253370654cdf6eaa13e56
Tuan Anh Le <anh.le@myob.com>
2021-09-03 20:54:29 +1000

    update test case to check payload data

--- src/test/kotlin/tv/codealong/tutorials/springboot/thenewboston/controllers/BankControllerTest.kt
+++ src/test/kotlin/tv/codealong/tutorials/springboot/thenewboston/controllers/BankControllerTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
@@ -20,7 +21,11 @@ internal class BankControllerTest {
         // when / then
         mockMvc.get("/api/banks")
             .andDo { print() }
-            .andExpect { status { isOk() } }
+            .andExpect {
+                status { isOk() }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath( "[0].accountNumber") { value("1234") }
+            }
     }
 
 }
