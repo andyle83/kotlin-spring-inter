77e0403d504cbd2e1c3d3a79bba55385c96b8a72
Tuan Anh Le <anh.le@myob.com>
2021-09-03 12:19:35 +1000

    initial commit

---
+++ src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/TheNewBostonApplication.kt
@@ -0,0 +1,11 @@
+package tv.codealong.tutorials.springboot.thenewboston
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TheNewBostonApplication
+
+fun main(args: Array<String>) {
+    runApplication<TheNewBostonApplication>(*args)
+}
