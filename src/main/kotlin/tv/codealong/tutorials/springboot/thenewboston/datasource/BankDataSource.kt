6a978039c1daabfe00cf8efc1108450a747904da
Tuan Anh Le <anh.le@myob.com>
2021-09-04 08:55:22 +1000

    code improvement

--- src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/datasource/BankDataSource.kt
+++ src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/datasource/BankDataSource.kt
@@ -3,6 +3,9 @@ package tv.codealong.tutorials.springboot.thenewboston.datasource
 import tv.codealong.tutorials.springboot.thenewboston.models.Bank
 
 interface BankDataSource {
+
     fun getBanks(): Collection<Bank>
+
     fun getBank(accountNumber: String): Bank
+
 }
