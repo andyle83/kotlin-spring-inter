8911b32e0d5797ea7e7c798be30da04f20333e8e
Tuan Anh Le <anh.le@myob.com>
2021-09-04 08:59:31 +1000

    code improvement

--- src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/datasource/mock/MockBankDataSource.kt
+++ src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/datasource/mock/MockBankDataSource.kt
@@ -7,7 +7,7 @@ import tv.codealong.tutorials.springboot.thenewboston.models.Bank
 @Repository
 class MockBankDataSource : BankDataSource {
 
-    val banks = listOf<Bank>(
+    val banks = listOf(
         Bank(accountNumber = "1234", trust = 3.14, transactionFee = 10),
         Bank(accountNumber = "1111", trust = 3.12, transactionFee = 5),
         Bank(accountNumber = "2222", trust = 3.2, transactionFee = 15)
